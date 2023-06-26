1349d23da8149514352f196dab66055f799cc96d
Anastasiia <dessertkiev@gmail.com>
2023-06-26 16:53:56 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,40 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Notiflix from "notiflix";
 import Section from "./Section/Section";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
 import Filter from "./Filter/Filter";
 
-
-export default function App(){
+export default function App() {
   const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+    () => JSON.parse(localStorage.getItem("contacts")) ?? []
   );
-  const [filter, setFilter] = useState('');
-  const visibleContacts = contacts.filter(contact =>
+  const [filter, setFilter] = useState("");
+  const visibleContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter)
   );
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
+    localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
 
-  const formSubmitHandler = newContact => {
+  const formSubmitHandler = (newContact) => {
     const isExists = contacts.some(
-      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
     if (isExists) {
       return Notiflix.Notify.warning(
         `${newContact.name} is already in contacts`
       );
     }
-    setContacts(prev => [...prev, newContact]);
+    setContacts((prev) => [...prev, newContact]);
   };
 
-  const deleteHandler = contactId => {
-    setContacts(prev => prev.filter(contact => contact.id !== contactId));
+  const deleteHandler = (contactId) => {
+    setContacts((prev) => prev.filter((contact) => contact.id !== contactId));
   };
 
-  const filterHandler = e => {
+  const filterHandler = (e) => {
     const { value } = e.currentTarget;
     setFilter(value.toLowerCase());
   };
@@ -53,4 +52,3 @@ export default function App(){
     </>
   );
 }
-
