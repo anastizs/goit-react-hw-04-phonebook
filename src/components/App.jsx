c39fd929c1dce303c241255649b13e2bdf5b2e52
Anastasiia <dessertkiev@gmail.com>
2023-06-26 15:46:47 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,56 @@
-import React, { Component } from "react";
+import { useState, useEffect} from "react";
 import Notiflix from "notiflix";
 import Section from "./Section/Section";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
 import Filter from "./Filter/Filter";
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-  };
 
-  formSubmitHandler = (newContact) => {
-    const isExists = this.state.contacts.some(
-      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
+export default function App(){
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const formSubmitHandler = newContact => {
+    const isExists = contacts.some(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
     if (isExists) {
       return Notiflix.Notify.warning(
         `${newContact.name} is already in contacts`
       );
     }
-    this.setState(({ contacts }) => {
-      return { contacts: [newContact, ...contacts] };
-    });
+    setContacts(prev => [...prev, newContact]);
   };
-  deleteHandler = (contactId) => {
-    this.setState(({ contacts }) => {
-      return {
-        contacts: contacts.filter((contact) => contact.id !== contactId),
-      };
-    });
+
+  const deleteHandler = contactId => {
+    setContacts(prev => prev.filter(contact => contact.id !== contactId));
   };
-  filterHandler = (e) => {
+
+  const filterHandler = e => {
     const { value } = e.currentTarget;
-    this.setState({ filter: value });
+    setFilter(value.toLowerCase());
   };
 
-  render() {
-    const filter = this.state.filter.toLowerCase();
-    const contacts = this.state.contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter)
-    );
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={formSubmitHandler} />
+      </Section>
 
-    return (
-      <>
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.formSubmitHandler} />
-        </Section>
-
-        <Section title="Contacts">
-          <Filter value={this.state.filter} onChange={this.filterHandler} />
-          <ContactList contacts={contacts} onDelete={this.deleteHandler} />
-        </Section>
-      </>
-    );
-  }
+      <Section title="Contacts">
+        <Filter value={filter} onChange={filterHandler} />
+        <ContactList contacts={visibleContacts} onDelete={deleteHandler} />
+      </Section>
+    </>
+  );
 }
-export default App;
+
